3a7d0a3ae68f14490987e22f80a6f950a34a6fb0
Kuba <kuba.nowak1993@gmail.com>
2017-11-24 15:17:57 +0100

    Making it work in ROS, progress 30%

--- scripts/map.py
+++ scripts/map.py
@@ -1,21 +1,25 @@
+#!/usr/bin/env python
+import rospy
+import rospkg
 import cv2
 import numpy as np
-import time
-
-
-class Pose:
-    def __init__(self, x, y, z):
-        self.x = x
-        self.y = y
-        self.z = z
+from sensor_msgs.msg import LaserScan
+from geometry_msgs.msg import PoseStamped, Pose
 
 
 class Map:
     def __init__(self):
+        rospy.init_node("map")
         self.pose = None
         self.scan = None
-        self.map_file = "map_image/lab_maze_2017.png"
-        self.maze_file = "maze.txt"
+        self.scan_sub = rospy.Subscriber("/scan", LaserScan, self.scanCallback)
+        self.pose_sub = rospy.Subscriber("/localization/pose", PoseStamped, self.poseCallback)
+
+        rospack = rospkg.RosPack()
+        path_maze = rospack.get_path('ras_maze_map')
+        path_map = rospack.get_path('ras_map')
+        self.map_file = path_map + "/map_image/lab_maze_2017.png"
+        self.maze_file = path_maze + "/maps/lab_maze_2017.txt"
 
         self.map = cv2.imread(self.map_file, 0)
         self.cell_size = 0.01
@@ -38,11 +42,11 @@ class Map:
         cv2.waitKey(1)
 
     def update(self):
+        if self.scan is None:
+            return
         pxy = np.array([self.scan[0] * np.cos(self.scan[1] + self.pose2d[2]),
                         self.scan[0] * np.sin(self.scan[1] + self.pose2d[2])]) + self.pose2d[:2, np.newaxis]
         p = (pxy / self.cell_size).astype(int)
-        print p
-        print self.pose2d
         pose_cell = (int(self.pose2d[0]/self.cell_size), int(self.pose2d[1]/self.cell_size))
         im = cv2.imread(self.map_file, 0).astype(int)
         cv2.line(im,
@@ -62,45 +66,27 @@ class Map:
         self.map = im.astype(np.uint8)
         cv2.imwrite(self.map_file, self.map)
 
-    def setPose2d(self, pose):
-        self.pose2d = np.array([pose.x, pose.y, 2 * np.arcsin(pose.z)])
-
     def scanCallback(self, msg):
-        self.scan = msg
+        self.scan = np.array([[msg.ranges], [i for i in range( int( round((msg.angle_max-msg.angle_min)/msg.angle_increment) ) + 1)]])
         print "Received scan"
 
     def poseCallback(self, msg):
         self.pose = msg.pose
+        self.pose2d = np.array([pose.pose.position.x, pose.pose.position.y, 2*np.arcsin(pose.pose.orientation.z)])
         print "Received pose"
 
-A = np.array([[0,1,2],[3,4,5],[6,7,8]])
-B = np.array([[1, 1, 2],[2, 2, 2]])
-A[(B[0], B[1])]+=1
-A[A > 8] = 8
-
-pi = 3.14159
-
-p = (2,2)
-b = 3
-a = np.ones((10, 10))
-a[((p[1]-b) if (p[1]-b>=0) else 0): ((p[1]+b+1) if (p[1]+b<10) else 10),
-   ((p[0]-b) if (p[0]-b>=0) else 0): ((p[0]+b+1) if (p[0]+b<10) else 10)] -= 10
 
 if __name__ == '__main__':
     m = Map()
 
+    rate = rospy.Rate(10)
     try:
         while True:
-            m.pose = Pose(0.2, 1.4, 0.0)
-            m.setPose2d(m.pose)
-            m.scan = np.array([[0.3, 0.31, 0.32, 0.33, 0.34, 0.35, 0.36, 0.37],
-                               [pi / 180 * 0, pi / 180 * 1, pi / 180 * 2, pi / 180 * 3, pi / 180 * 4, pi / 180 * 5,
-                                pi / 180 * 6, pi / 180 * 7]])
-
             m.update()
 
             cv2.imshow('image', cv2.resize(m.map, None, fx=2, fy=2))
-            cv2.waitKey(10)
-            time.sleep(0.4)
-    finally:
+            cv2.waitKey(1)
+            rate.sleep()
+    except rospy.ROSInterruptException:
         cv2.destroyAllWindows()
+        pass
