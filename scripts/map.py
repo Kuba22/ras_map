0f482f5982941ade88323b5e0f941f85f583391d
Kuba <kuba.nowak1993@gmail.com>
2017-12-02 23:57:17 +0100

    Condition map update on average distance from scan to its nearest neighbor on the map

--- scripts/map.py
+++ scripts/map.py
@@ -5,8 +5,13 @@ import cv2
 import numpy as np
 from sensor_msgs.msg import LaserScan
 from geometry_msgs.msg import PoseStamped, Pose
+from sklearn.neighbors import NearestNeighbors
 
-def drawMap(im, pose_cell):
+increment_rate = 12
+decrement_rate = 3
+threshold = 100
+
+def drawMap(im, pose_cell, name):
     im = im.astype(np.uint8)
     cv2.line(im,
 (pose_cell[0] + 5, pose_cell[1] - 5),
@@ -14,8 +19,33 @@ def drawMap(im, pose_cell):
     cv2.line(im,
 (pose_cell[0] + 5, pose_cell[1] + 5),
 (pose_cell[0] - 5, pose_cell[1] - 5), 255, 1)
-    cv2.imshow('image', cv2.resize(im, None, fx=2, fy=2))
+    cv2.imshow(name, cv2.resize(im, None, fx=3, fy=3))
     cv2.waitKey(1)
+
+def boundp(p, h, w):
+    p[0, p[0, :]<0]=0
+    p[0, p[0, :]>=w]=w-1
+    p[1, p[1, :]<0]=0
+    p[1, p[1, :]>=h]=h-1
+    return p
+
+def prepIm(im, p, pose_cell):
+    im[p[1], p[0]] += increment_rate
+    for i in range(p.shape[1]):
+        angle = np.arctan2(p[1, i]-pose_cell[1], p[0, i]-pose_cell[0])
+        r = int(np.sqrt((p[0,i]-pose_cell[0])*(p[0,i]-pose_cell[0]) + (p[1,i]-pose_cell[1])*(p[1,i]-pose_cell[1])))
+        c = np.cos(angle)
+        s = np.sin(angle)
+        x = (pose_cell[0] + np.arange(1, r-1, 1)*c).astype(int)
+        y = (pose_cell[1] + np.arange(1, r-1, 1)*s).astype(int)
+        im[ y, x ] -= decrement_rate
+    im[im > 255] = 255
+    im[im < 0] = 0
+    im[0, :] = 255
+    im[-1, :] = 255
+    im[:, 0] = 255
+    im[:, -1] = 255
+    return im
 
 class Map:
     def __init__(self):
@@ -30,10 +60,11 @@ class Map:
         path_maze = rospack.get_path('ras_maze_map')
         path_map = rospack.get_path('ras_map')
         self.map_file = path_map + "/map_image/lab_maze_2017.png"
+        self.map_file_th = path_map + "/map_image/map.png"
         self.maze_file = path_maze + "/maps/lab_maze_2017.txt"
 
         self.map = cv2.imread(self.map_file, 0)
-        self.cell_size = 0.01
+        self.cell_size = 0.02
         self.b = int(0.5 / self.cell_size)
 
         maze_file = open(self.maze_file)
@@ -43,13 +74,18 @@ class Map:
         # min_x = min(min(lines[:,0]), min(lines[:, 2])) # min x
         # min_y = min(min(lines[:,1]), min(lines[:, 3])) # min y
         self.w = int(np.ceil(max_x / self.cell_size))
-        self.h = int(np.ceil(max_y / self.cell_size))    
+        self.h = int(np.ceil(max_y / self.cell_size))
+        lines_img = (lines / self.cell_size).astype(int)
+        mp = np.zeros((self.h, self.w))
+        for ln in lines_img:
+            cv2.line(mp, (ln[0], ln[1]), (ln[2], ln[3]), 120, 1)
+        self.map_cells = np.where(mp==120)
+        self.map_cells = np.array([self.map_cells[0], self.map_cells[1]])
         if self.map is None:
-            lines_img = (lines / self.cell_size).astype(int)
-            self.map = np.zeros((self.h, self.w))
-            for ln in lines_img:
-                cv2.line(self.map, (ln[0], ln[1]), (ln[2], ln[3]), 255, 1)
-            cv2.imwrite(self.map_file, self.map)
+            self.map = mp
+        cv2.imwrite(self.map_file, self.map)
+        cv2.imwrite(self.map_file_th, self.map)
+
         cv2.imshow('image', self.map)
         cv2.waitKey(1)
 
@@ -64,36 +100,28 @@ class Map:
         pxy = np.array([self.scan[0] * np.cos(self.scan[1] + self.pose2d[2]),
                         self.scan[0] * np.sin(self.scan[1] + self.pose2d[2])]) + self.pose2d[:2, np.newaxis]
         p = (pxy / self.cell_size).astype(int)
-        p[0, p[0, :]<0]=0
-        p[0, p[0, :]>=self.w]=self.w-1
-        p[1, p[1, :]<0]=0
-        p[1, p[1, :]>=self.h]=self.h-1
+        im = cv2.imread(self.map_file, 0).astype(int)
 
-        im = cv2.imread(self.map_file, 0)
-        im = im.astype(int)
-        im[p[1], p[0]] += 10
+        err = self.getErr(self.map_cells, p)
+        if err < 4:
+            p = boundp(p, self.h, self.w)
+            im = prepIm(im, p, pose_cell).astype(np.uint8)
+            self.map = im
+            cv2.imwrite(self.map_file, self.map)
+            th = np.zeros((self.h, self.w))
+            th[im>threshold] = 255
+            cv2.imwrite(self.map_file_th, th)
+            drawMap(th, pose_cell, 'thresholded')
 
-        for i in range(p.shape[1]):
-            angle = np.arctan2(p[1, i]-pose_cell[1], p[0, i]-pose_cell[0])
-            r = int(np.sqrt((p[0,i]-pose_cell[0])*(p[0,i]-pose_cell[0]) + (p[1,i]-pose_cell[1])*(p[1,i]-pose_cell[1])))
-            c = np.cos(angle)
-            s = np.sin(angle)
-            x = (pose_cell[0] + np.arange(1, r-1, 1)*c).astype(int)
-            y = (pose_cell[1] + np.arange(1, r-1, 1)*s).astype(int)
-            im[ y, x ] -= 4
+        drawMap(im, pose_cell, 'image')
 
-        im[im > 255] = 255
-        im[im < 0] = 0
-        im[0, :] = 255
-        im[-1, :] = 255
-        im[:, 0] = 255
-        im[:, -1] = 255
-
-        self.map = im.astype(np.uint8)
-        cv2.imwrite(self.map_file, self.map)
-        drawMap(im, pose_cell)
-        
-
+    def getErr(self, map_cells, scan_cells):
+        p = scan_cells.T.astype(np.float32)
+        m = map_cells.T.astype(np.float32)
+        nn = NearestNeighbors(n_neighbors=1).fit(m)
+        d, i = nn.kneighbors(p)
+        return np.mean(d)
+    
     def scanCallback(self, msg):
         ranges = list(msg.ranges)
         bearings = [i*np.pi/180 for i in range( int( round((msg.angle_max-msg.angle_min)/msg.angle_increment) ) + 1)]
